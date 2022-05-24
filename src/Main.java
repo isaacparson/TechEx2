493a09c1c0a13330f2ef9aa96002574fd19ab202
Dani <mptts28@gmail.com>
2022-05-24 19:30:11 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,45 @@
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 public class Main {
-
     //Ввод с клавиатуры
     public static String enter() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
     }
-    /*
-    public static String getInfo(String Race, char Ch) {
-        try {
-            File file = new File("info.txt");
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
-    }
-     */
     //Замена букв на цифры
-    public static StringBuilder poleNum(String Race, String Pole) throws IOException {
-        String PoleA, PoleB, PoleC;
-        StringBuilder PoleNum = new StringBuilder(16);
-
+    public static String poleNum(String Race, String Pole) throws IOException {
+        String PoleNum = Pole;
         File file = new File("C:\\Users\\Dani\\IdeaProjects\\TechEx2\\src\\info.txt");
-        //создаем объект FileReader для объекта File
         FileReader fr = new FileReader(file);
-        //создаем BufferedReader с существующего FileReader для построчного считывания
         BufferedReader reader = new BufferedReader(fr);
-        // считаем сначала первую строку
         String line = reader.readLine();
-        System.out.println(line);
-        for (int i = 0; i < 16; i++) {
-            if (line.equals(Race)) {
-                for (int j = 1; j < 16; j++) {
-                    line = reader.readLine();
-                    System.out.println(line);
-                    if (line.equals(Pole.charAt(i))) {
-                        line = reader.readLine();
-                        System.out.println(line);
-                        PoleNum.setCharAt(i, line.charAt(0));
-                        break;
-                    }
-                }
+        if (line.equals(Race)) {
+            for (int i = 0; i < 4; i++) {
+                line = reader.readLine();
+                char where = line.charAt(0);
+                line = reader.readLine();
+                char Num = line.charAt(0);
+                PoleNum = PoleNum.replace(where, Num);
+            }
+        }
+        else {
+            for (int i = 0; i < 100; i++) {
+                line = reader.readLine();
+                if (line.equals(Race)) break;
+            }
+            for (int i = 0; i < 4; i++) {
+                line = reader.readLine();
+                char where = line.charAt(0);
+                line = reader.readLine();
+                char Num = line.charAt(0);
+                PoleNum = PoleNum.replace(where, Num);
             }
 
         }
+
         return PoleNum;
     }
 
